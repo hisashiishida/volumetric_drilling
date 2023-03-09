e097a63d8cc1cc1c9631705fd03e8afe0d7903d4
Hisashi Ishida <hishida3@jhu.edu>
2023-03-09 17:46:53 -0500

    Add color edt

--- scripts/EDTImageGeneration/Edtvisual.py
+++ scripts/EDTImageGeneration/Edtvisual.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-# img_ori = Image.open('./../../edt_grids_RT147_256/EAC_256/edtplane_42.png')
+# img_ori = Image.open('./../../resources/edt_grids/EAC_256/edtplane_42.png')
 # img_ori = Image.open('./../../edt_grids_RT147_256/TMJ_256/edtplane_42.png')
 # img_ori = Image.open('./../../edt_grids_RT147_256/Sinus_+_Dura_256/edtplane_42.png')
 img_ori = Image.open('./../../resources/edt_grids/sinus_1209_256/Segments1_256/edtplane_42.png')
@@ -25,11 +25,18 @@ for x in range(img.shape[0]):
 
 ax = plt.subplot()
 im = ax.imshow(img_new)
+
+# X = np.linspace(0,img_new.shape[0])
+# Y = np.linspace(0,img_new.shape[1])
+
+# im = ax.contourf(X, Y, img_new)
+im = ax.contourf(img_new)
 # create an Axes on the right side of ax. The width of cax will be 5%
 # of ax and the padding between cax and ax will be fixed at 0.05 inch.
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
 
 plt.colorbar(im, cax=cax)
+# plt.colorbar(im)
 
 plt.show()
